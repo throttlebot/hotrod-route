244a64432c61fc7a07754b43c5cae039f46f61f3
Will Wang <willwang@Wills-MacBook-Pro.local>
2018-06-08 17:17:51 -0700

    fixed test assert values

--- route_unit_test.py
+++ route_unit_test.py
@@ -7,8 +7,8 @@ def test(host, port):
 	response = requests.get("http://" + host + ":" + str(port) + "/route?dropoff=test&pickup=test2")
 	assert response.status_code == 200, "Status code was {} instead of 200".format(response.status_code)
 	result = response.json()
-	assert result["Pickup"] == "test"
-	assert result["Dropoff"] == "test2"
+	assert result["Pickup"] == "test2"
+	assert result["Dropoff"] == "test"
 	assert isinstance(result["ETA"], int)
 
 if __name__ == '__main__':
